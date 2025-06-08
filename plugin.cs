b94fac8a7d308e7f4bc802687d5c7a419dcc2edd
zazarick <danyamalyshev2023@gmail.com>
2025-06-08 17:25:09 +0300

    Update and rename pkugin.cs to plugin.cs

---
+++ plugin.cs
@@ -0,0 +1,60 @@
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Server;
+using MEC;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SCPSwap
+{
+    public class SCPSwapPlugin : Plugin<SCPSwapConfig>
+    {
+        public override string Name => "SCPSwap";
+        public override string Author => "Zazar";
+        public override string Prefix => "scpswap";
+        public override System.Version Version => new System.Version(1, 0, 0);
+
+        public static SCPSwapPlugin Singleton;
+
+        public HashSet<int> SwappedPlayers = new HashSet<int>();
+        public bool SwapEnabled = false;
+
+        private CoroutineHandle timerCoroutine;
+
+        public override void OnEnabled()
+        {
+            Singleton = this;
+            Exiled.Events.Handlers.Server.RoundStarted += OnRoundStarted;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStarted;
+            Singleton = null;
+            base.OnDisabled();
+        }
+
+        private void OnRoundStarted()
+        {
+            SwappedPlayers.Clear();
+            SwapEnabled = true;
+            if (timerCoroutine.IsRunning)
+                Timing.KillCoroutines(timerCoroutine);
+            timerCoroutine = Timing.RunCoroutine(DisableSwapAfterTime());
+
+            // Броадкаст только SCP
+            string message = Config.StartBroadcast.Replace("{time}", Config.SwapTimeSeconds.ToString());
+            foreach (var ply in Player.List)
+            {
+                if (ply.Role.Team == PlayerRoles.Team.SCPs)
+                    ply.Broadcast(8, message, Broadcast.BroadcastFlags.Normal, true);
+            }
+        }
+
+        private IEnumerator<float> DisableSwapAfterTime()
+        {
+            yield return Timing.WaitForSeconds(Config.SwapTimeSeconds);
+            SwapEnabled = false;
+        }
+    }
+}
